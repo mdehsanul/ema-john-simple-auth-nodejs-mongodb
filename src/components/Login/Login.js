b778373c09cfaeb5eec0048739667e3d33587c57
mdehsanul <mdehsanulhaqueshanto04@gmail.com>
2021-04-06 00:56:31 +0600

    firebase-auth done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,12 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
+import { useContext } from "react";
 import { UserContext } from "../../App";
-import { useHistory, useLocation } from "react-router";
+import { useHistory, useLocation } from "react-router-dom";
 import {
   initializeLoginFramework,
-  GoogleSignIn,
-  GoogleSignOut,
-  FacebookSignIn,
-  GitHubSignIn,
+  handleGoogleSignIn,
+  handleSignOut,
+  handleFbSignIn,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "./loginManager";
@@ -14,41 +14,44 @@ import {
 function Login() {
   const [newUser, setNewUser] = useState(false);
   const [user, setUser] = useState({
-    isSignIn: false,
+    isSignedIn: false,
     name: "",
     email: "",
     password: "",
     photo: "",
-    error: "",
-    success: false,
   });
 
   initializeLoginFramework();
 
-  const [logInUser, setLogInUser] = useContext(UserContext);
-
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   const history = useHistory();
-  const loaction = useLocation();
-  let { from } = loaction.state || { from: { pathname: "/" } };
+  const location = useLocation();
+  let { from } = location.state || { from: { pathname: "/" } };
 
-  const handleResponse = (response, redirect) => {
-    setUser(response);
-    setLogInUser(response);
+  const googleSignIn = () => {
+    handleGoogleSignIn().then((res) => {
+      handleResponse(res, true);
+    });
+  };
+
+  const fbSignIn = () => {
+    handleFbSignIn().then((res) => {
+      handleResponse(res, true);
+    });
+  };
+
+  const signOut = () => {
+    handleSignOut().then((res) => {
+      handleResponse(res, false);
+    });
+  };
+
+  const handleResponse = (res, redirect) => {
+    setUser(res);
+    setLoggedInUser(res);
     if (redirect) {
       history.replace(from);
     }
-  };
-
-  const handleGoogleSignIn = () => {
-    GoogleSignIn().then((response) => {
-      handleResponse(response, true);
-    });
-  };
-
-  const handleGitHubSignIn = () => {
-    GitHubSignIn().then((response) => {
-      handleResponse(response, true);
-    });
   };
 
   // -------------------------------------------------------------------- Start Implementation Mnually ---------------------------------------------------------------------------------
@@ -99,9 +102,20 @@ function Login() {
   return (
     // ------------------------------ start firebase --------------------------------------
     <div style={{ textAlign: "center" }}>
-      <button onClick={handleGoogleSignIn}>Sign In</button>
+      {user.isSignedIn ? (
+        <button onClick={signOut}>Sign Out</button>
+      ) : (
+        <button onClick={googleSignIn}>Sign In</button>
+      )}
       <br />
-      <button onClick={handleGitHubSignIn}>Log in using GitHub</button>
+      <button onClick={fbSignIn}>Sign in using Facebook</button>
+      {user.isSignedIn && (
+        <div>
+          <p>Welcome, {user.name}!</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
 
       {/*--------------------------- End firebase ------------------------------------------*/}
       {/*--------------------------- Start Manual ------------------------------------------*/}
--- src/components/Login/loginManager.js
+++ src/components/Login/loginManager.js
@@ -14,57 +14,75 @@ export const initializeLoginFramework = () => {
 };
 
 // Google Sign In using firebase -----------------------------------------
-export const GoogleSignIn = () => {
-  // Create an instance of the Google provider object
+export const handleGoogleSignIn = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
-  // To sign in with a Google pop-up window, call signInWithPopup
   return firebase
     .auth()
     .signInWithPopup(googleProvider)
-    .then((result) => {
-      const { displayName, photoURL, email } = result.user;
-      const SignedInUser = {
-        isSignIn: true,
+    .then((res) => {
+      const { displayName, photoURL, email } = res.user;
+      const signedInUser = {
+        isSignedIn: true,
         name: displayName,
         email: email,
         photo: photoURL,
         success: true,
       };
-      return SignedInUser;
+      return signedInUser;
     })
-    .catch((error) => {
-      console.log(error);
-      console.log(error.message);
+    .catch((err) => {
+      console.log(err);
+      console.log(err.message);
     });
 };
 
 // GitHub Sign In using firebase -----------------------------------------
-export const GitHubSignIn = () => {
-  // Create an instance of the GitHub provider object
-  var githubProvider = new firebase.auth.GithubAuthProvider();
-  // To sign in with a GitHub pop-up window, call signInWithPopup:
+export const handleFbSignIn = () => {
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
   return firebase
     .auth()
-    .signInWithPopup(githubProvider)
-    .then((result) => {
-      const user = result.user;
+    .signInWithPopup(fbProvider)
+    .then(function (result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
       user.success = true;
       return user;
     })
-    .catch((error) => {
-      console.log(error.code, error.message);
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+};
+
+// SignOut
+export const handleSignOut = () => {
+  return firebase
+    .auth()
+    .signOut()
+    .then((res) => {
+      const signedOutUser = {
+        isSignedIn: false,
+        name: "",
+        email: "",
+        photo: "",
+        error: "",
+        success: false,
+      };
+      return signedOutUser;
+    })
+    .catch((err) => {
+      // An error happened.
     });
 };
 
 export const createUserWithEmailAndPassword = (name, email, password) => {
-  // creating User With Email And Password in firebase (a little work with firebase when working  manually)
   return firebase
     .auth()
     .createUserWithEmailAndPassword(email, password)
-    .then((response) => {
-      // Signed in
-      const newUserInfo = response.user;
-      newUserInfo.error = " ";
+    .then((res) => {
+      const newUserInfo = res.user;
+      newUserInfo.error = "";
       newUserInfo.success = true;
       updateUserName(name);
       return newUserInfo;
@@ -81,13 +99,13 @@ export const signInWithEmailAndPassword = (email, password) => {
   return firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
-    .then((response) => {
-      const newUserInfo = response.user;
-      newUserInfo.error = " ";
+    .then((res) => {
+      const newUserInfo = res.user;
+      newUserInfo.error = "";
       newUserInfo.success = true;
       return newUserInfo;
     })
-    .catch((error) => {
+    .catch(function (error) {
       const newUserInfo = {};
       newUserInfo.error = error.message;
       newUserInfo.success = false;
@@ -96,17 +114,16 @@ export const signInWithEmailAndPassword = (email, password) => {
 };
 
 const updateUserName = (name) => {
-  var user = firebase.auth().currentUser;
+  const user = firebase.auth().currentUser;
+
   user
     .updateProfile({
       displayName: name,
     })
     .then(function () {
-      // Update successful.
-      console.log("User name updated successfully");
+      console.log("user name updated successfully");
     })
     .catch(function (error) {
-      // An error happened.
       console.log(error);
     });
 };
